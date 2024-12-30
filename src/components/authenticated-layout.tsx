9cd1be8c313209ca6bb8d797d174f0f0e9957b77
Harun29 <ibrahimovic.harun01@gmail.com>
2024-12-30 02:07:58 +0100

    changed top bar to stickey

--- src/components/authenticated-layout.tsx
+++ src/components/authenticated-layout.tsx
@@ -30,7 +30,7 @@ const AuthenticatedLayout = ({ children }: { children: React.ReactNode }) => {
       <AppSidebar />
       <SidebarInset>
         <header
-          className="bg-[#f7f7f7] dark:bg-[#0a0a0a] flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 relative"
+          className="bg-[#f7f7f7] dark:bg-[#0a0a0a] flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 sticky top-0"
         >
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1 z-50" />
