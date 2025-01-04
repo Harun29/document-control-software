d48a1b88576bcc472d36087ac398c00c5f893fd7
Harun29 <ibrahimovic.harun01@gmail.com>
2025-01-04 21:15:29 +0100

    refactor: enhance date formatting and clean up tooltip components for document actions

--- src/app/docs/allDocsColumns.tsx
+++ src/app/docs/allDocsColumns.tsx
@@ -51,7 +51,13 @@ export const columns = (
         <div>
           {isNaN(date.getTime())
             ? "Invalid Date"
-            : date.toLocaleString("en-GB")}
+            : date.toLocaleString("en-GB", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+              })}
         </div>
       );
     },
@@ -87,19 +93,21 @@ export const columns = (
       console.log(row.original.orgID, usersOrg);
       return (
         <div className="flex space-x-4 justify-self-end">
-          {usersOrg === row.original.org && <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger
-                className="transition-transform transform hover:scale-125 duration-300 ease-in-out"
-                onClick={() => handleModifyDoc(row.original)}
-              >
-                <Pencil strokeWidth={1} />
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Modify</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>}
+          {usersOrg === row.original.org && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger
+                  className="transition-transform transform hover:scale-125 duration-300 ease-in-out"
+                  onClick={() => handleModifyDoc(row.original)}
+                >
+                  <Pencil strokeWidth={1} />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Modify</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger
@@ -115,46 +123,48 @@ export const columns = (
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
-          {usersOrg === row.original.org && <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger
-                className="transition-transform transform hover:scale-125 duration-300 ease-in-out"
-                onClick={() => handleDeleteDoc(row.original)}
-              >
-                <AlertDialog>
-                  <AlertDialogTrigger asChild>
-                    <Button variant="destructive">
-                      <Trash className="w-4 h-4" />
-                    </Button>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                      <AlertDialogDescription className="flex flex-col gap-2">
-                        Do you want to delete this document?{" "}
-                        {row.original?.title}
-                      </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                      <AlertDialogCancel>Cancel</AlertDialogCancel>
-                      <AlertDialogAction
-                        disabled={loadingAction}
-                        onClick={() => handleDeleteDoc(row.original!)}
-                      >
-                        {loadingAction && (
-                          <LoaderCircle className="w-4 h-4 animate-spin" />
-                        )}
-                        Delete
-                      </AlertDialogAction>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Delete</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>}
+          {usersOrg === row.original.org && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger
+                  className="transition-transform transform hover:scale-125 duration-300 ease-in-out"
+                  onClick={() => handleDeleteDoc(row.original)}
+                >
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant="destructive">
+                        <Trash className="w-4 h-4" />
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                        <AlertDialogDescription className="flex flex-col gap-2">
+                          Do you want to delete this document?{" "}
+                          {row.original?.title}
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                          disabled={loadingAction}
+                          onClick={() => handleDeleteDoc(row.original!)}
+                        >
+                          {loadingAction && (
+                            <LoaderCircle className="w-4 h-4 animate-spin" />
+                          )}
+                          Delete
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Delete</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
         </div>
       );
     },
