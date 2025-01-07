f5644295e71bac28d397b287a48673d07471e6a4
Harun29 <ibrahimovic.harun01@gmail.com>
2025-01-07 22:37:59 +0100

    refactor: improve code formatting and simplify sidebar navigation logic

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import * as React from "react";
-import { BookOpen, History, Home, Settings2, User2, Users2 } from "lucide-react";
+import {
+  BookOpen,
+  History,
+  Home,
+  Settings2,
+  User2,
+  Users2,
+} from "lucide-react";
 import { FileTextIcon } from "lucide-react";
 import { NavMain } from "@/components/nav-main";
 import { NavUser } from "@/components/nav-user";
@@ -25,8 +32,8 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const createOrgRef = useRef<HTMLDivElement | null>(null);
   const createUserRef = useRef<HTMLDivElement | null>(null);
   const { user } = useAuth();
-  const {isAdmin} = useAuth();
-  const {isEditor} = useAuth();
+  const { isAdmin } = useAuth();
+  const { isEditor } = useAuth();
 
   const data = {
     teams: [
@@ -41,50 +48,70 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         url: "/",
         icon: Home,
       },
-      ...(isAdmin ? [{ 
-        title: "Departments",
-        url: "/orgs",
-        icon: Users2,
-        isActive: true
-      }] : []),
-      ...(isAdmin ? [{
-        title: "Users",
-        icon: User2,
-        url: "/users",
-      }] : []),
-      ...((isEditor || isAdmin) ? [
-        {
-        title: "Documents",
-        icon: FileTextIcon,
-        items:[
-          {
-            title: "Manage documents",
-            url: "/docs",
-          },
-          {
-            title: "Add document",
-            url: "/docs/create",
-          },
-          {
-            title: "Document requests",
-            url: "/docs/requests",
-          }
-        ]
-        }
-      ]:[{
-        title: "Documents",
-        icon: FileTextIcon,
-        items:[
-          {
-            title: "Add document",
-            url: "/docs/create",
-          },
-          {
-            title: "View documents",
-            url: "/docs",
-          }
-        ]
-      }]),
+      ...(isAdmin
+        ? [
+            {
+              title: "Departments",
+              url: "/orgs",
+              icon: Users2,
+              isActive: true,
+            },
+          ]
+        : []),
+      ...(isAdmin
+        ? [
+            {
+              title: "Users",
+              icon: User2,
+              url: "/users",
+            },
+          ]
+        : []),
+      ...(isEditor
+        ? [
+            {
+              title: "Documents",
+              icon: FileTextIcon,
+              items: [
+                {
+                  title: "Manage documents",
+                  url: "/docs",
+                },
+                {
+                  title: "Add document",
+                  url: "/docs/create",
+                },
+                {
+                  title: "Document requests",
+                  url: "/docs/requests",
+                },
+              ],
+            },
+          ]
+        : isAdmin
+        ? [
+            {
+              title: "Documents",
+              icon: FileTextIcon,
+              url: "/docs",
+            },
+          ]
+        : [
+            {
+              title: "Documents",
+              icon: FileTextIcon,
+              items: [
+                {
+                  title: "Add document",
+                  url: "/docs/create",
+                },
+                {
+                  title: "View documents",
+                  url: "/docs",
+                },
+              ],
+            },
+          ]),
       {
         title: "History",
         url: "/history",
@@ -138,18 +165,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={navItems} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={{
-          firstName: user?.userInfo?.firstName ?? "",
-          lastName: user?.userInfo?.lastName ?? "",
-          email: user?.userInfo?.email ?? "",
-          org: user?.userInfo?.org ?? "",
-          orgName: user?.userInfo?.orgName ?? "",
-          role: user?.userInfo?.role ?? ""
-        }} />
+        <NavUser
+          user={{
+            firstName: user?.userInfo?.firstName ?? "",
+            lastName: user?.userInfo?.lastName ?? "",
+            email: user?.userInfo?.email ?? "",
+            org: user?.userInfo?.org ?? "",
+            orgName: user?.userInfo?.orgName ?? "",
+            role: user?.userInfo?.role ?? "",
+          }}
+        />
       </SidebarFooter>
       <SidebarRail />
       {createUser && <CreateUserCard ref={createUserRef} />}
     </Sidebar>
   );
 }
-
